0bb0c606fcf036ce92413a67c99ceaa4bdbd46e5
TakuTahran <takurama@gmail.com>
2024-09-08 18:44:52 -0400

    bb

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import CountdownTimer from './Components/CountdownTimer/CountdownTimer';
 function App() {
   return (
     <div className="App">
+      <h2> Til you're stuck with me 24/7 ❤️❤️❤️</h2>
      <CountdownTimer
      countdownTimestampMs={1727740800000}/>
     </div>
